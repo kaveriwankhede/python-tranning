449dca4e84173045d39d1f30f8cf660c0bcd8121
Kaveriwankhede@1234 <kaveriwankhede>
2026-06-04 22:44:29 -0700

    Day-6:url_for etc

---
+++ app1.py
@@ -0,0 +1,56 @@
+from flask import Flask , render_template
+app = Flask(__name__)
+stud=[
+    {
+        'name':'John Doe',
+        'roll_no':1001,
+        'marks':90,
+    },
+    {
+        'name':'Jane Smith',
+        'roll_no':1002,
+        'marks':22,
+    },
+    {
+        'name':'Alice Johnson',
+        'roll_no':1003,
+        'marks':19,
+    },
+    {
+        'name':'Bob Brown',
+        'roll_no':1004,
+        'marks':21,
+    }
+]
+@app.route('/')
+def Home():
+    return render_template('Home.html',students=stud)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/students')
+def students():
+    return render_template('students.html', students=stud)
+
+@app.route('/Subject')
+def Subject():
+    return render_template('Subject.html')
+
+@app.route('/Theory')
+def Theory():
+    return render_template('Theory.html')
+
+@app.route('/MCQ')
+def MCQ():
+    return render_template('MCQ.html')
+
+@app.route('/Result')
+def Result():
+    return render_template('Result.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
